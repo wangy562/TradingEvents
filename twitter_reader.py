f84f3b998dc40e659bd0cfe4896e76a29cd405e4
wangy562 <andyyqwang@outlook.com>
2022-05-04 16:36:50 -0700

    make twitter api calls

--- twitter_reader.py
+++ twitter_reader.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, requests
 from dotenv import load_dotenv
 
 
@@ -12,6 +12,7 @@ class TwitterReader:
         self.list_id = os.getenv("TWITTER_LIST_ID")
         self.tickers = {}
         self.tweet_list = []
+        self.api_url = f'https://api.twitter.com/2/lists/{self.list_id}/tweets'
 
     def generate_data(self):
         if os.path.isfile('./tickers.json') is False:
@@ -35,9 +36,18 @@ class TwitterReader:
         f.close()
 
     def load_api(self):
-        pass
+        if requests.get(self.api_url, headers={'Authorization': f'Bearer {self.bearer}'}).status_code == 200:
+            return True
+
+        return False
 
     def get_tweets(self):
+        response = requests.get(self.api_url, headers={'Authorization': f'Bearer {self.bearer}'}).json()
         for ticker in self.tickers:
-            pass 
-
+            for tweet in response['data']:
+                tweet_id = tweet['id']
+                if tweet['id'] not in self.tweet_list:
+                    if ticker in tweet['text']:
+                        pass
+                    self.tweet_list.append(tweet_id)
+                    pass
