6e8c1a511de748ef70b0820b543ea2b5022711ac
wangy562 <andyyqwang@outlook.com>
2022-05-03 17:54:31 -0700

    create add and delete tickers functions

--- twitter_reader.py
+++ twitter_reader.py
@@ -6,10 +6,12 @@ class TwitterReader:
 
     def __init__(self):
         load_dotenv()
-        self.api_key = os.environ.get('TWITTER_API_KEY')
-        self.api_secret = os.environ.get('TWITTER_API_SECRET')
-        self.bearer = os.environ.get('TWITTER_BEARER_TOKEN')
+        self.api_key = os.getenv('TWITTER_API_KEY')
+        self.api_secret = os.getenv('TWITTER_API_SECRET')
+        self.bearer = os.getenv('TWITTER_BEARER_TOKEN')
+        self.list_id = os.getenv("TWITTER_LIST_ID")
         self.tickers = {}
+        self.tweet_list = []
 
     def generate_data(self):
         if os.path.isfile('./tickers.json') is False:
@@ -17,5 +19,25 @@ class TwitterReader:
                 json.dumps(self.tickers, f)
             f.close()
 
-    def load_tickers(self):
+    def add_ticker(self, ticker):
+        with open('tickers.json', 'x') as f:
+            self.tickers = json.loads(f)
+        if ticker not in self.tickers.keys():
+            self.tickers[ticker] = ticker
+        json.dumps(self.tickers, f)
+        f.close()
+
+    def delete_ticker(self, ticker):
+        with open('tickers.json', 'x') as f:
+            self.tickers = json.loads(f)
+        self.ticker.pop(ticker, None)
+        json.dumps(self.tickers, f) 
+        f.close()
+
+    def load_api(self):
         pass
+
+    def get_tweets(self):
+        for ticker in self.tickers:
+            pass 
+
